a67b11b292bb6393281333d2d9ebf93b49727de5
juan jose lopez roldan <jjlr1994@gmail.com>
2017-11-05 22:22:50 +0100

    introducion de parametros

--- creacion_usuario.sh
+++ creacion_usuario.sh
@@ -7,4 +7,10 @@ ruta="ou=People,dc=lopez,dc=gonzalonazareno,dc=org"
 grupo="2000"
 clave="clave del administrador de LDAP"
 
+read -p "Introduce un nombre de usuario sin tildes: " nombre
+read -p "Introduce los dos apellidos: " apellido
+read -p "Introduce nombre real completo y apellidos: " nombreapellido
+read -p "Introduce tu nombre de pila: " pila
+read -p "Introduce tu correo electronico: " correo
+
 
